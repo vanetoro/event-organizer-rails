892ce46ff813c533564d1c25d39a5026526705c0
Vanessa Toro <vanetoro84@gmail.com>
2018-07-24 17:59:20 -0400

    removed key and sececret and replaced with ENV variables

--- config/initializers/ominauth.rb
+++ config/initializers/ominauth.rb
@@ -1,5 +1,5 @@
 OmniAuth.config.logger = Rails.logger
 
 Rails.application.config.middleware.use OmniAuth::Builder do
-  provider :google_oauth2, '483828829678-ksbabpitm9uc9nr7o5t9476pqmu81v9q.apps.googleusercontent.com', 'JVH6TEbg8ENM7UY4Kyg9s6GW', {client_options: {ssl: {ca_file: Rails.root.join("cacert.pem").to_s}}}
+  provider :google_oauth2, ENV['GOOGLE_KEY'],ENV['GOOGLE_SECRET'], {client_options: {ssl: {ca_file: Rails.root.join("cacert.pem").to_s}}}
 end
