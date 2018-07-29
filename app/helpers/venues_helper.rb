e1424abb757f257c996f26c9a90046922f6ae5ed
Vanessa Toro <vanetoro84@gmail.com>
2018-07-29 19:59:32 -0400

    added helper 'display_num_of_events' to refactor venue show page

--- app/helpers/venues_helper.rb
+++ app/helpers/venues_helper.rb
@@ -1,2 +1,10 @@
 module VenuesHelper
+
+  def   display_num_of_events(venue )
+    if venue.events.count > 0
+      "# of events #{venue.events.count}"
+    else
+      'Currently there are no events at this venue'
+    end
+  end
 end
